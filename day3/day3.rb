cd167abd1c76439158a8b90c5e18acb66d6b7c81
Brian <bbbrianboylen@gmail.com>
2021-12-09 20:59:53 -0800

    day 3 complete

--- day3/day3.rb
+++ day3/day3.rb
@@ -56,3 +56,75 @@ puts gamma_num
 puts epsilon_num
 
 puts gamma_num * epsilon_num
+
+
+# Part 2
+
+def generate_rating(main, side) 
+  
+  nums = File.read("input").split
+  length = nums[0].length
+  length.times do |i|
+    main_array = []
+    side_array = []
+    main_count = 0
+    side_count = 0
+
+    nums.each do |code|
+      if code[i] == main
+        main_array << code
+        main_count += 1
+      else
+        side_array << code
+        side_count += 1
+      end
+    end
+    # print main_array
+    if main_count >= side_count 
+      nums = main_array
+    else
+      nums = side_array
+    end
+    # puts nums, i
+    if nums.length == 1 then return nums[0]
+  end
+end
+end
+
+def generate_rating2(main, side) 
+  
+  nums = File.read("input").split
+  length = nums[0].length
+  length.times do |i|
+    main_array = []
+    side_array = []
+    main_count = 0
+    side_count = 0
+
+    nums.each do |code|
+      if code[i] == main
+        main_array << code
+        main_count += 1
+      else
+        side_array << code
+        side_count += 1
+      end
+    end
+    # print main_array
+    if main_count <= side_count 
+      nums = main_array
+    else
+      nums = side_array
+    end
+    # puts nums, i
+    if nums.length == 1 then return nums[0]
+  end
+end
+end
+
+oxygen = convert(generate_rating("1","0"))
+co2 =  convert(generate_rating2("0","1"))
+
+puts oxygen, co2
+
+puts oxygen * co2
