172ae8b83f972e1851e56199f7f82759d466722a
Brian <bbbrianboylen@gmail.com>
2021-12-11 10:17:36 -0800

    parsing added day4

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,3 +1,16 @@
-input = File.read("input").split("\n\n")
-drawn_numbers = input.shift
-squares = input
+require "csv"
+require "set"
+
+file = File.open("input")
+data = file.readlines.map(&:chomp)
+file.close
+
+draws = CSV.parse(data[0]).first.map(&:to_i)
+print data[1]
+boards = data.drop(2).each_slice(6).to_a.map(
+  &->(x) { x.delete_if(&:empty?).map(&->(r) { r.split.map(&->(c) { {d: c.to_i, m: false}})})}
+)
+
+def runBingo(draws, boards)
+  
+end
