d45d7259c3b5c955d73d7ebf9b145d25d0a7ae84
Brian <bbbrianboylen@gmail.com>
2021-12-11 11:17:22 -0800

    day4 complete

--- day4/day4.rb
+++ day4/day4.rb
@@ -12,5 +12,41 @@ boards = data.drop(2).each_slice(6).to_a.map(
 )
 
 def runBingo(draws, boards)
-  
+  results = []
+  draws.each do |x|
+    boards.each_with_index do |b, bi|
+      b.each do |r|
+        r.each_with_index do |c, i|
+          if x == c[:d]
+            c[:m] = true
+            if b.map(&->(rr) { rr[i] }).all? { |cc| cc[:m] == true } || r.all? { |rr| rr[:m] == true }
+              sum = 0
+              boards[bi].each do |r|
+                r.each do |c|
+                  if c[:m] == false
+                    sum += c[:d]
+                  end
+                end
+              end
+              results.push({board_idx: bi, draw: x, unmarked_sum: sum})
+            end
+          end
+        end
+      end
+    end
+  end
+  results
 end
+
+board_firsts = []
+seen = Set[]
+runBingo(draws, boards).each do |r|
+  if !seen.include?(r[:board_idx])
+    seen.add(r[:board_idx])
+    board_firsts.push(r)
+  end
+end
+# part one
+pp board_firsts.first[:draw] * board_firsts.first[:unmarked_sum]
+# part one
+pp board_firsts.last[:draw] * board_firsts.last[:unmarked_sum]
