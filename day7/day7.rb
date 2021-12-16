4b91bfa3d5cb6ea1a92dead479227d4de8784e9b
Brian <bbbrianboylen@gmail.com>
2021-12-15 21:59:29 -0800

    day7 part 2 done

--- day7/day7.rb
+++ day7/day7.rb
@@ -5,7 +5,6 @@ crab_list = File.read("input").split(",").map {|crab|
 
 
 sorted_list = crab_list.sort
-print sorted_list.length
 
 def find_median(list)
   mid = ((list.length-1)/2.0).floor
@@ -22,4 +21,28 @@ def calc_fuel (array, median)
 end
 
 puts calc_fuel(sorted_list, median)
-puts calc_fuel(crab_list, median)
+
+## PART 2
+
+def find_mean(list)
+  sum = 0
+  list.each do |num|
+    sum += num
+  end
+  sum/list.length
+end
+
+mean = find_mean(crab_list)
+
+def calc_fuel2 (array, mean)
+  fuel = 0
+  array.each do |pos|
+    diff = (mean - pos).abs
+    diff.times do |i|
+      fuel += i + 1
+    end
+  end
+  fuel
+end
+
+puts calc_fuel2(sorted_list, mean)
