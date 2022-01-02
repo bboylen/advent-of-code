30a017d5ace540f856c97eb39ee90b680e9610a4
Brian <bbbrianboylen@gmail.com>
2022-01-01 22:15:19 -0800

    day10 complete

--- day10/day10_p2.rb
+++ day10/day10_p2.rb
@@ -5,37 +5,67 @@ def read_input(input)
 end
 
 def iterate_lines(input)
-  corrupted = []
+  not_corrupted = []
   input.each do |line|
-    corrupted << find_corrupted(line)
+    not_corrupted << line if !find_corrupted(line)
   end
-  corrupted
+  not_corrupted
 end
 
 def find_corrupted(line)
   closing_syntax = { ']' => '[', '>' => '<', ')' => '(', '}' => '{' }
   stack = []
+  is_corrupted = false
 
   line.each_char do |char|
     if closing_syntax.include?(char)
       if stack.last && stack.last == closing_syntax[char]
         stack.pop
       else
-        return char
+        return true
       end
     else
       stack << char
     end
   end
-  nil
+  is_corrupted
+end
+
+def find_complete(line)
+  closing_syntax = { ']' => '[', '>' => '<', ')' => '(', '}' => '{' }
+  stack = []
+  complete = []
+  
+  line.each_char do |char|
+    if closing_syntax.include?(char)
+      if stack.last && stack.last == closing_syntax[char]
+        stack.pop
+      else
+        return true
+      end
+    else
+      stack << char
+    end
+  end
+  stack.reverse_each { |x| complete << closing_syntax.invert[x]}
+  complete
+end
+
+def complete(lines)
+  output = []
+  lines.each do |line|
+    output << find_complete(line)
+  end
+  output
 end
 
 def main(input)
-  points = { ']' => 57, '>' => 25_137, ')' => 3, '}' => 1197 }
+  points = { ']' => 2, '>' => 4, ')' => 1, '}' => 3 }
 
   lines = read_input(input)
-  corrupted = iterate_lines(lines)
-  print corrupted.filter { |char| !char.nil? }.reduce(0) { |sum, char| sum + points[char] }
+  not_corrupted = iterate_lines(lines)
+  completion = complete(not_corrupted)
+  print completion.map {|end_chars| end_chars.reduce(0) { |prod, char| prod * 5 + points[char] }}.sort[(completion.length-1)/2]
 end
 
 main('input')
